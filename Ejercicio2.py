13bb9dcea500cd062f5c73576b4d26e9c147558c
agomezmartin <agomezmartinpro@gmail.com>
2023-05-26 18:55:22 +0200

    Ejercicio 1

---
+++ Ejercicio2.py
@@ -0,0 +1,10 @@
+# 2. Dispones de una frase y una letra,
+# solicitados al usuario y
+# debes mostrar la cantidad de veces
+# que aparece la letra en la frase.
+
+frase = input('Inserta una frase')
+letra = input('Inserta una letra')
+
+
+
